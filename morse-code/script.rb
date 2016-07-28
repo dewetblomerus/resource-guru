61cc39c0c3bb43e0fd4d0f055ef73b45a47c71d4
De Wet <dewet@blomerus.org>
2016-07-28 21:23:35 +0200

    a little more rubocop

--- morse-code/script.rb
+++ morse-code/script.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/morse_code'
 
-File.open("encoded_obfuscated_message.txt", 'w') do |file|
+File.open('encoded_obfuscated_message.txt', 'w') do |file|
   File.readlines('plain_text_message.txt').each do |line|
     file.puts Main.prepare_to_send(line)
   end
